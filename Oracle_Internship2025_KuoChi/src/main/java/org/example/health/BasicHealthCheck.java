d044125302045efcd098203052207bb8a40683b0
freddie.c.kuo <freddie.c.kuo@gmail.com>
2025-01-05 17:28:00 +0800

    Update BasicHealthCheck.java

--- Oracle_Internship2025_KuoChi/src/main/java/org/example/health/BasicHealthCheck.java
+++ Oracle_Internship2025_KuoChi/src/main/java/org/example/health/BasicHealthCheck.java
@@ -3,8 +3,12 @@ package org.example.health;
 import com.codahale.metrics.health.HealthCheck;
 import javax.xml.transform.Result;
 
+@Path("/api")
+@Produces(MediaType.APPLICATION_JSON)
 public class BasicHealthCheck extends HealthCheck {
-    @Override
+
+    @GET
+    @Path("/health")
     protected Result check() throws Exception {
         return HealthCheck.Result.healthy();
     }
