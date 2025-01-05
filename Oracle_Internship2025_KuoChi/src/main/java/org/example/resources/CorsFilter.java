8fd54def2adc269c2e69eb173b89b48a3004eca8
freddie.c.kuo <freddie.c.kuo@gmail.com>
2025-01-05 17:56:19 +0800

    CorsFilter IP UPdate

--- Oracle_Internship2025_KuoChi/src/main/java/org/example/resources/CorsFilter.java
+++ Oracle_Internship2025_KuoChi/src/main/java/org/example/resources/CorsFilter.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class CorsFilter implements ContainerResponseFilter {
     @Override
     public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
-        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://localhost:3000");
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "http://54.169.198.77:3000");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
         responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
         responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
